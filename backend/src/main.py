f149804f8764f3b50be6fcd7fbe5279b22220821
Ben Sippel <brs6412@rit.edu>
2025-08-08 21:50:35 -0400

    Changed CORS allow origins to allow straight from npm or docker angular container

--- backend/src/main.py
+++ backend/src/main.py
@@ -7,6 +7,8 @@ from datetime import datetime
 app = FastAPI()
 
 origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
     "http://localhost:4200",
     "http://127.0.0.1:4200"
 ]
