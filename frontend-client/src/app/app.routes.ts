a59350d729d39a943cfe4643763094f5caa9fbf7
Ben Sippel <brs6412@rit.edu>
2025-08-23 16:43:06 -0400

    Remove frontend work from backend commit

--- frontend-client/src/app/app.routes.ts
+++ frontend-client/src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
-import { Home } from './components/home/home';
-import { Iocs } from './components/iocs/iocs';
-import { Users } from './components/users/users';
-import { PageNotFound } from './components/page-not-found/page-not-found';
 
-export const routes: Routes = [
-    { path: '', component: Home },
-    { path: 'iocs', component: Iocs },
-    { path: 'users', component: Users },
-    { path: '**', component: PageNotFound }
-];
+export const routes: Routes = [];
