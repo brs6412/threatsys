83bed1298db67d99402cb46e1d793e9584b67e74
Ben Sippel <brs6412@rit.edu>
2025-08-23 14:24:03 -0400

    Adds init files to server layers

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,9 +1,9 @@
-from ..database import Base
-from .organization import Organization
-from .role import Role
-from .user import User
-from .ioc_type import IOCType
-from .ioc import IOC
-from .ioc_relationship import IOCRelationship
+from src.database import Base
+from src.models.organization import Organization
+from src.models.role import Role
+from src.models.user import User
+from src.models.ioc_type import IOCType
+from src.models.ioc import IOC
+from src.models.ioc_relationship import IOCRelationship
 
 __all__ = ["Base", "Organization", "Role", "User", "IOCType", "IOC", "IOCRelationship"]
---
+++ backend/src/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .user import *
+from .organization import *
+from .role import *
+from .ioc import *
